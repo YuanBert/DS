f5520d992cd593347d93915f1c327cb6bba25b3e
Bert <zgenyuan@hotmail.com>
2018-05-24 18:12:33 +0800

    添加定时器库文件
    暂实现函数的功能，采用三个定时器，一个定时器来进行地感检测，一个定时器进行日志上报，另一个定时器进行PWM输出，来进行LED灯进行控制。

---
+++ src/tim.c
@@ -0,0 +1,9 @@
+/*
+ * tim.c
+ *
+ *  Created on: 2018Äê5ÔÂ24ÈÕ
+ *      Author: bertz
+ */
+#include "tim.h"
+
+
---
+++ src/tim.h
@@ -0,0 +1,13 @@
+/*
+ * tim.h
+ *
+ *  Created on: 2018Äê5ÔÂ24ÈÕ
+ *      Author: bertz
+ */
+
+#ifndef TIM_H_
+#define TIM_H_
+#include "stm32f10x.h"
+
+
+#endif /* TIM_H_ */
